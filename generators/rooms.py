ee91abfeacd18574c85128a5410795fb7f7fce1a
Jakub Musiał <jmusial2003@gmail.com>
2023-01-14 22:39:44 +0100

    Deleted generators/

--- generators/rooms.py
+++
@@ -1,9 +0,0 @@
-with open("./docs/rooms.txt", 'w') as file:
-    file.write("INSERT INTO Rooms(s_max) VALUES\n")
-    for _ in range(4):
-        file.write("\t(50),\n")
-    for _ in range(4):
-        file.write("\t(80),\n")
-    for _ in range(2):
-        file.write("\t(100),\n")
-    file.write(f"\t(100);")
